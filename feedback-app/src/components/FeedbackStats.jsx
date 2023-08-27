6404fee48bf121c32bcee8b69690e86ca3da45c9
danutdotrar <morarasu.danut@gmail.com>
2023-08-27 13:19:03 +0300

    Set average decimal toFixed 1

--- feedback-app/src/components/FeedbackStats.jsx
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -10,11 +10,13 @@ const FeedbackStats = ({ feedback }) => {
     let average =
         feedback.reduce((acc, val) => acc + val.rating, 0) / feedback.length;
 
+    average = average.toFixed(1);
+
     return (
         <>
             <div className="feedback-stats">
                 <h4>{feedback.length} Reviews</h4>
-                <h4>Average rating: {average}</h4>
+                <h4>Average rating: {isNaN(average) ? "0" : average}</h4>
             </div>
         </>
     );
