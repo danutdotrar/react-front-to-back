403c10e8054b1086b4152b47d910d2881f3e1904
danutdotrar <morarasu.danut@gmail.com>
2023-08-28 20:45:23 +0300

    Add RatingSelect Component

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,18 +1,38 @@
 import { useState } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
 const FeedbackForm = () => {
     const [text, setText] = useState("");
+    const [rating, setRating] = useState(10);
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState("");
+
     const handleTextChange = (e) => {
+        if (text === "") {
+            setBtnDisabled(true);
+            setMessage(null);
+        } else if (text.trim().length <= 10) {
+            setBtnDisabled(true);
+            setMessage("At least 10 characters");
+        } else if (text.trim().length >= 10) {
+            setMessage(null);
+            setBtnDisabled(false);
+        }
+
         setText(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
     };
 
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
-                {/* todo - rating select component */}
+                <RatingSelect select={(rating) => setRating(rating)} />
                 <div className="input-group">
                     <input
                         type="text"
@@ -22,12 +42,13 @@ const FeedbackForm = () => {
                     />
                     <Button
                         type={"submit"}
-                        isDisabled={false}
+                        isDisabled={btnDisabled}
                         version={"primary"}
                     >
                         Send
                     </Button>
                 </div>
+                {message && <div className="message">{message}</div>}
             </form>
         </Card>
     );
