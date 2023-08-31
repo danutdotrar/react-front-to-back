0c31f89a93a2d16acaf8c828ea33f032e3b60e20
danutdotrar <morarasu.danut@gmail.com>
2023-08-31 17:56:25 +0300

    Add starter files

---
+++ github-finder/src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+    return (
+        <>
+            <div className="bg-purple-500">
+                <h1 className="text-3xl underline">Hello world!</h1>
+                <button className="btn">Click me!</button>
+            </div>
+        </>
+    );
+};
+
+export default App;
