9ab0019ac6a94851aac771d45a8e2b0fe87cc2df
danutdotrar <morarasu.danut@gmail.com>
2023-09-12 12:03:08 +0300

    Add search user functionality

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -4,25 +4,53 @@ import githubReducer from "./GithubReducer";
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
     const initialState = {
         users: [],
-        loading: true,
+        loading: false,
     };
 
     const [state, dispatch] = useReducer(githubReducer, initialState);
 
-    const fetchUsers = async () => {
-        setLoading();
-        const response = await fetch(`${GITHUB_URL}/users`);
+    // const fetchUsers = async () => {
+    //     setLoading();
+    //     const response = await fetch(`${GITHUB_URL}/users`, {
+    //         headers: {
+    //             Authorization: `token ${GITHUB_TOKEN}`,
+    //         },
+    //     });
 
-        const data = await response.json();
+    //     const data = await response.json();
+
+    //     console.log(data);
+
+    //     dispatch({
+    //         type: "GET_USERS",
+    //         payload: data,
+    //     });
+    // };
+
+    const searchUsers = async (text) => {
+        setLoading();
+
+        const params = new URLSearchParams({ q: text });
+
+        const response = await fetch(
+            `https://${GITHUB_URL}/search/users?${params}`,
+            {
+                headers: {
+                    Authorization: `token ${GITHUB_TOKEN}`,
+                },
+            }
+        );
+
+        const { items } = await response.json();
 
         dispatch({
             type: "GET_USERS",
-            payload: data,
+            payload: items,
         });
     };
 
@@ -30,7 +58,11 @@ export const GithubProvider = ({ children }) => {
 
     return (
         <GithubContext.Provider
-            value={{ users: state.users, loading: state.loading, fetchUsers }}
+            value={{
+                users: state.users,
+                loading: state.loading,
+                searchUsers,
+            }}
         >
             {children}
         </GithubContext.Provider>
