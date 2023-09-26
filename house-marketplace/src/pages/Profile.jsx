baa51497cdfd7eba3ee1ee71553b822014b41e1c
danutdotrar <morarasu.danut@gmail.com>
2023-09-26 11:10:21 +0300

    Show user profile name

--- house-marketplace/src/pages/Profile.jsx
+++ house-marketplace/src/pages/Profile.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { getAuth } from "firebase/auth";
+import { useState, useEffect } from "react";
 
 const Profile = () => {
-    return (
-        <div>
-            <h1>Profile</h1>
-        </div>
-    );
+    const auth = getAuth();
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        console.log(auth.currentUser);
+        setUser(auth.currentUser);
+    }, []);
+
+    return user ? <h1>{user.displayName}</h1> : "Not Logged In";
 };
 
 export default Profile;
