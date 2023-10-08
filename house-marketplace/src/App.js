c125beb8a204c822da3f81d3d34697f64cc64dbb
danutdotrar <morarasu.danut@gmail.com>
2023-10-08 20:01:39 +0300

    Add private route

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import NavBar from "./components/NavBar";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
     return (
@@ -16,7 +17,9 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Explore />} />
                     <Route path="/offers" element={<Offers />} />
-                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/profile" element={<PrivateRoute />}>
+                        <Route path="/profile" element={<Profile />} />
+                    </Route>
                     <Route path="/sign-in" element={<SignIn />} />
                     <Route path="/sign-up" element={<SignUp />} />
                     <Route
@@ -30,5 +33,7 @@ const App = () => {
         </>
     );
 };
+
+NEXT 14. 9 - Forgot Password Page
 
 export default App;
