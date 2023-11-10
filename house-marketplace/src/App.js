4e58baf9ed1c4a56052b070bd540c609a9c4da3b
danutdotrar <morarasu.danut@gmail.com>
2023-11-10 16:07:37 +0200

    Add explore page start

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -9,6 +9,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import NavBar from "./components/NavBar";
 import PrivateRoute from "./components/PrivateRoute";
+import Category from "./pages/Category";
 
 const App = () => {
     return (
@@ -17,6 +18,10 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Explore />} />
                     <Route path="/offers" element={<Offers />} />
+                    <Route
+                        path="/category/:categoryName"
+                        element={<Category />}
+                    />
                     <Route path="/profile" element={<PrivateRoute />}>
                         <Route path="/profile" element={<Profile />} />
                     </Route>
