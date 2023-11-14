4d77f1f857d05327589384daee1d1d8d4f68c752
danutdotrar <morarasu.danut@gmail.com>
2023-11-14 17:11:46 +0200

    Add Offers page content

--- house-marketplace/src/components/NavBar.jsx
+++ house-marketplace/src/components/NavBar.jsx
@@ -56,7 +56,7 @@ const NavBar = () => {
                                     : "navbarListItemName"
                             )}
                         >
-                            Offer
+                            Offers
                         </p>
                     </li>
                     <li
