416f64727fb35111dd2e420216758ab56c6393b2
danutdotrar <morarasu.danut@gmail.com>
2023-11-20 17:57:59 +0200

    Add user to formData

--- house-marketplace/src/pages/CreateListing.jsx
+++ house-marketplace/src/pages/CreateListing.jsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { Spinner } from "../components/Spinner";
 
 function CreateListing() {
+    const [geoLocationEnabled, setGeoLocationEnabled] = useState(true);
     const [formData, setFormData] = useState({
         type: "rent",
         name: "",
@@ -16,6 +20,27 @@ function CreateListing() {
         latitude: 0,
         longitude: 0,
     });
+
+    const auth = getAuth();
+    const navigate = useNavigate();
+    const isMounted = useRef(true);
+
+    useEffect(() => {
+        if (isMounted) {
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    setFormData({ ...formData, userRef: user.uid });
+                } else {
+                    navigate("/sign-in");
+                }
+            });
+        }
+
+        return () => {
+            isMounted.current = false;
+        };
+    }, [isMounted]);
+
     return <div>CreateListing</div>;
 }
 
