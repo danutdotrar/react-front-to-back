8219c2667b9e5e52cb78ebb623adee47f3f981a2
danutdotrar <morarasu.danut@gmail.com>
2023-11-20 19:17:09 +0200

    Add form elements

--- house-marketplace/src/pages/CreateListing.jsx
+++ house-marketplace/src/pages/CreateListing.jsx
@@ -59,6 +59,11 @@ function CreateListing() {
         };
     }, [isMounted]);
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+    const onMutate = (e) => {};
+
     if (loading) {
         return (
             <>
@@ -66,7 +71,156 @@ function CreateListing() {
             </>
         );
     }
-    return <div>CreateListing</div>;
+    return (
+        <div className="profile">
+            <header>
+                <p className="pageHeader">Create a Listing</p>
+            </header>
+            <main>
+                <form onSubmit={onSubmit}>
+                    <label className="formLabel">Sell / Rent</label>
+                    <div className="formButtons">
+                        <button
+                            type="button"
+                            className={
+                                type === "sale"
+                                    ? "formButtonActive"
+                                    : "formButton"
+                            }
+                            id="type"
+                            value="sale"
+                            onClick={onMutate}
+                        >
+                            Sell
+                        </button>
+                        <button
+                            type="button"
+                            className={
+                                type === "rent"
+                                    ? "formButtonActive"
+                                    : "formButton"
+                            }
+                            id="type"
+                            value="rebt"
+                            onClick={onMutate}
+                        >
+                            Rent
+                        </button>
+                    </div>
+
+                    <label className="formLabel">Name</label>
+                    <input
+                        className="formInputName"
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={onMutate}
+                        maxLength="32"
+                        minLength="10"
+                        required
+                    />
+
+                    <div className="formRooms flex">
+                        <div>
+                            <label className="formLabel">Bedrooms</label>
+                            <input
+                                className="formInputSmall"
+                                type="number"
+                                id="bedrooms"
+                                value={bedrooms}
+                                onChange={onMutate}
+                                min="1"
+                                max="50"
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label className="formLabel">Bathrooms</label>
+                            <input
+                                className="formInputSmall"
+                                type="number"
+                                id="bathrooms"
+                                value={bathrooms}
+                                onChange={onMutate}
+                                min="1"
+                                max="50"
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <label className="formLabel">Parking spot</label>
+                    <div className="formButtons">
+                        <button
+                            className={
+                                parking ? "formButtonActive" : "formButton"
+                            }
+                            type="button"
+                            id="parking"
+                            value={true}
+                            onClick={onMutate}
+                            min="1"
+                            max="50"
+                        >
+                            Yes
+                        </button>
+                        <button
+                            className={
+                                !parking && parking !== null
+                                    ? "formButtonActive"
+                                    : "formButton"
+                            }
+                            type="button"
+                            id="parking"
+                            value={false}
+                            onClick={onMutate}
+                        >
+                            No
+                        </button>
+                    </div>
+
+                    <label className="formLabel">Furnished</label>
+                    <div className="formButtons">
+                        <button
+                            className={
+                                furnished ? "formButtonActive" : "formButton"
+                            }
+                            type="button"
+                            id="furnished"
+                            value={true}
+                            onClick={onMutate}
+                        >
+                            Yes
+                        </button>
+                        <button
+                            className={
+                                !furnished && furnished !== null
+                                    ? "formButtonActive"
+                                    : "formButton"
+                            }
+                            type="button"
+                            id="furnished"
+                            value={false}
+                            onClick={onMutate}
+                        >
+                            No
+                        </button>
+                    </div>
+
+                    <label className="formLabel">Address</label>
+                    <textarea
+                        className="formInputAddress"
+                        type="text"
+                        id="address"
+                        value={address}
+                        onChange={onMutate}
+                        required
+                    ></textarea>
+                </form>
+            </main>
+        </div>
+    );
 }
 
 export default CreateListing;
